adfd8cbf5ec9658f08298ad8d37963e9bd31035b
Asutosh Sahoo <asutosh.sahoo@quantiphi.com>
2019-02-14 12:26:25 +0530

    Google charts basic integration @graph_ui/google-charts-timeline

--- graph_ui/urls.py
+++ graph_ui/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^d3-timeline', views.d3_timeline),
+    url(r'^google-charts-timeline', views.google_charts_timeline),
 ]
--- graph_ui/views.py
+++ graph_ui/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def d3_timeline(request):
     return render(request, 'graph_ui/d3-timeline-home.html')
+
+def google_charts_timeline(request):
+    return render(request, 'graph_ui/google-charts-timeline-home.html')
