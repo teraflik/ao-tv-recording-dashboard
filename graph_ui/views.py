841cca4a1412431b72250b2b999bd9868858845d
Asutosh Sahoo <asutosh.sahoo@quantiphi.com>
2019-02-15 12:54:32 +0530

    Displaying all channel slots in homepage via django-templates.

--- graph_ui/views.py
+++ graph_ui/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-# Create your views here.
+from rest_api import models
 
+# Getting the list of channels
+channels = models.ChannelInfo.objects.all()
+
+# Create your views here.
 def d3_timeline(request):
     return render(request, 'graph_ui/d3-timeline-home.html')
 
 def google_charts_timeline(request):
-    return render(request, 'graph_ui/google-charts-timeline-home.html')
+    return render(request, 'graph_ui/google-charts-timeline-home.html', {'channels': channels})
