53abebc199fe17e786ae7ee75da09d364b14fb22
Asutosh Sahoo <asutosh.sahoo@quantiphi.com>
2019-02-19 12:56:51 +0530

    Serving /ui/request_id_filter/ hits from ui.views.general

--- ui/urls.py
+++ ui/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^get/', views.general),
+    url(r'^request_id_filter/', views.general),
     url(r'^home', views.home),
     url(r'', views.tables),
 ]
