cd9154b8664e885fa3f8a6485d9f6b73e8c700d7
Asutosh Sahoo <asutosh.sahoo@quantiphi.com>
2019-02-21 15:43:24 +0530

    Color followed by its label in label bar

--- graph_ui/static/graph_ui/js/google-charts-timeline-main.js
+++ graph_ui/static/graph_ui/js/google-charts-timeline-main.js
@@ -342,8 +342,6 @@ function populateTimeline(timeline, endpoint, index) {
                             minValue: startDate,
                             maxValue: endDate,
                             gridlines: {color: 'pink', count: 4},
-                            // ticks: [startDate, endDate]
-                            // ticks: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
                         },
                     width: '100%',
                     height: '105'
@@ -440,8 +438,8 @@ function setColorLabels() {
             var color = document.createElement("th");
             color.setAttribute("bgcolor", stageToColor[key]);
 
-            trElement.appendChild(stage);
             trElement.appendChild(color);
+            trElement.appendChild(stage);
         }
     }
 }
