46e5f0f0b3a76090862a8d77c4861a69843b5071
Asutosh Sahoo <asutosh.sahoo@quantiphi.com>
2019-02-27 12:51:11 +0530

    Added /api/blank/ to fetch entries from invalid_frame_tracking as per GET params.

--- rest_api/urls.py
+++ rest_api/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^recording/', views.RecordingView.as_view(), name='api-recording'),
     url(r'^recording_graph_ui_redirect/', views.GraphUIRedirectView.as_view()),
+    url(r'^blank/', views.BlankView.as_view(), name='api-blank'),
 ]
--- rest_api/views.py
+++ rest_api/views.py
@@ -48,13 +48,15 @@ class RecordingView(APIView):
         input_timezone = pytz.timezone(input_timezone)
         ist_timezone = pytz.timezone('Asia/Kolkata')
 
+        # construct the datetime objects by specifying the date, time and the timezone (using .localize()).
         start_datetime = input_timezone.localize(datetime.datetime.strptime(input_date + " " + input_start_time, "%Y-%m-%d %H:%M:%S.%f"))
         finish_datetime = input_timezone.localize(datetime.datetime.strptime(input_date + " " + input_finish_time, "%Y-%m-%d %H:%M:%S.%f"))
 
+        # keep 1 min buffer for starting and ending times.
         start_datetime = start_datetime - datetime.timedelta(minutes=1)
         finish_datetime = finish_datetime + datetime.timedelta(minutes=1)
         
-
+        # get the corresponding IST date and time values in string format 
         start_date_ist_str = start_datetime.astimezone(ist_timezone).strftime("%Y%m%d")
         start_time_ist_str = start_datetime.astimezone(ist_timezone).strftime("%H%M%S.%f")
 
@@ -80,6 +82,75 @@ class RecordingView(APIView):
         recordings = recordings.filter(q)        
         
         serializer = RecordingSerializer(recordings, many=True)
+        return Response(serializer.data)
+        # return JsonResponse(temp_json)
+    
+    def post(self, request):
+        pass
+
+class BlankView(APIView):
+    
+    def get(self, request):
+
+        # #   retrieve the GET parameters
+        device_id = request.GET.get('device_id') or 'both'
+        input_date = request.GET.get('date') or str(datetime.datetime.date(datetime.datetime.now()))
+
+
+        input_start_time = handle_buggy_time(request.GET.get('start_time')) or '00:00:00.000'
+        input_finish_time = handle_buggy_time(request.GET.get('finish_time')) or '23:59:59.999'
+        input_timezone = request.GET.get('timezone') or 'Asia/Kolkata'
+        channel_values = request.GET.getlist('channel_values') or list(map(str, ChannelInfo.objects.values_list('channel_value', flat=True)))
+
+        # temp_json = {
+        #     'device_id': device_id,
+        #     'input_date': input_date,
+        #     'input_start_time': input_start_time,
+        #     'input_finish_time': input_finish_time,
+        #     'input_timezone': input_timezone,
+        #     'channel_values': channel_values
+        # }
+        
+        input_timezone = pytz.timezone(input_timezone)
+        ist_timezone = pytz.timezone('Asia/Kolkata')
+
+        # construct the datetime object by specifying the date, time and the timezone (using .localize()).
+        start_datetime = input_timezone.localize(datetime.datetime.strptime(input_date + " " + input_start_time, "%Y-%m-%d %H:%M:%S.%f"))
+        finish_datetime = input_timezone.localize(datetime.datetime.strptime(input_date + " " + input_finish_time, "%Y-%m-%d %H:%M:%S.%f"))
+
+        # keep 1 min buffer for starting and ending times.
+        start_datetime = start_datetime - datetime.timedelta(minutes=1)
+        finish_datetime = finish_datetime + datetime.timedelta(minutes=1)
+        
+        # get the corresponding IST date and time values in string format.
+        start_date_ist_str = start_datetime.astimezone(ist_timezone).strftime("%Y%m%d")
+        start_time_ist_str = start_datetime.astimezone(ist_timezone).strftime("%H%M%S.%f")
+
+        finish_date_ist_str = finish_datetime.astimezone(ist_timezone).strftime("%Y%m%d")
+        finish_time_ist_str = finish_datetime.astimezone(ist_timezone).strftime("%H%M%S.%f")
+        
+        filters = {}
+        
+        if device_id != 'both':
+            filters['device_id'] = device_id
+        
+        invalid_frame_trackings = InvalidFrameTracking.objects.filter(**filters)
+
+        q = Q()
+        for ch in channel_values:
+            start = '_'.join([start_date_ist_str, ch, start_time_ist_str])
+            finish = '_'.join([finish_date_ist_str, ch, finish_time_ist_str])
+            
+            # print("")
+            # print("start request_id :- {}".format(start))
+            # print("finish request_id :- {}".format(finish))
+            # print("")
+
+            q = q | (Q(request_id__range=[start, finish]) & Q(request_id__contains = '_' + ch + '_'))
+
+        invalid_frame_trackings = invalid_frame_trackings.filter(q)        
+        
+        serializer = InvalidFrameTrackingSerializer(invalid_frame_trackings, many=True)
         return Response(serializer.data)
         # return JsonResponse(temp_json)
     
