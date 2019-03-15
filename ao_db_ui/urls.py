1e16b476769f6845853b09d09d446621dc02c62f
Asutosh Sahoo <asutosh.sahoo@quantiphi.com>
2019-03-15 14:45:12 +0530

    / redirects to /graph_ui/recording

--- ao_db_ui/urls.py
+++ ao_db_ui/urls.py
@@ -17,7 +17,9 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic import TemplateView
 from django.contrib.auth import views as auth_views
+from django.views.generic.base import RedirectView
 from ui.forms import LoginForm
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -26,4 +28,5 @@ urlpatterns = [
     url(r'^graph_ui/', include('graph_ui.urls')),
     url(r'^login/', auth_views.LoginView.as_view(template_name='ui/login.html', authentication_form=LoginForm, redirect_authenticated_user=True), name='login'),
     url(r'^logout/', auth_views.LogoutView.as_view(template_name='ui/logout.html'), name='logout'),
+    url(r'^.*$', RedirectView.as_view(url='/graph_ui/recording', permanent=False), name='index')
 ]
