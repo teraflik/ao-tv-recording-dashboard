d00ff9d5c964d32574843f96c673f308d5f902e8
Asutosh Sahoo <asutosh.sahoo@quantiphi.com>
2019-03-15 12:55:02 +0530

    Gave name to home url to view mapping.

--- ui/urls.py
+++ ui/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     url(r'^recording/', views.general, name='ui-recording'),
     url(r'^recording_graph_ui_redirect', views.general),
     url(r'^blank', views.general, name='ui-blank'),
-    url(r'^home', views.home),
+    url(r'^home', views.home, name='ui-home'),
 ]
