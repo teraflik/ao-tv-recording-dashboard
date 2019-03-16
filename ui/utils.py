84f35929fc65845d6a31549a792ff0e5ff116d64
Asutosh Sahoo <asutosh.sahoo@quantiphi.com>
2019-03-16 06:35:47 +0530

    Shifted helper methods to utils.py in UI app.

---
+++ ui/utils.py
@@ -0,0 +1,65 @@
+def make_table_dict(request):
+    current_url = resolve(request.path_info).url_name
+
+    # table_dict = dict(request.GET._iterlists())
+    table_dict = dict(request.GET)
+    
+    for key, value in table_dict.items():
+        if key != 'channel_values':
+            table_dict[key] = value[0]
+    
+    # map channel_values to channel_names and remove the channel_values key
+    if table_dict.get('channel_values'):
+        channel_values = [int(x) for x in table_dict['channel_values']]
+        channel_names = [t['channel_name'] for t in channels.filter(channel_value__in=channel_values)]
+        table_dict['channel_names'] = channel_names
+        table_dict.pop("channel_values", None)
+    
+    # Adding table information to dictionary
+    if current_url == 'ui-recording':
+        table_dict['table'] =  'Recording'
+    elif current_url == 'ui-blank':
+        table_dict['table'] =  'Invalid Frame Tracking'
+
+def make_table_html(table_dict):
+    
+    if not table_dict:
+        return ''
+    
+    table_head_html = ''
+    table_body_html = ''
+
+    if table_dict.get('table'):
+        table_head_html += '<th>Table</th>'
+        table_body_html += '<td>' + table_dict.get('table') + '</td>'
+
+    if table_dict.get('date'):
+        table_head_html += '<th>Date</th>'
+        table_body_html += '<td>' + table_dict.get('date') + '</td>'
+    
+    if table_dict.get('start_time') and table_dict.get('finish_time'):
+        table_head_html += '<th>Time Interval</th>'
+        table_body_html += '<td>' + table_dict.get('start_time') + ' - ' + table_dict.get('finish_time') + '</td>'
+    
+    if table_dict.get('timezone'):
+        table_head_html += '<th>Timezone</th>'
+        table_body_html += '<td>' + table_dict.get('timezone') + '</td>'
+    
+    if table_dict.get('channel_names'):
+        table_head_html += '<th>Channel Names</th>'
+        table_body_html += '<td>' + str(table_dict.get('channel_names')) + '</td>'
+    
+    if table_dict.get('device_id'):
+        table_head_html += '<th>Device</th>'
+        table_body_html += '<td>' + table_dict.get('device_id') + '</td>'
+    
+    if table_dict.get('request_id'):
+        table_head_html += '<th>Request ID</th>'
+        table_body_html += '<td>' + table_dict.get('request_id') + '</td>'
+
+    table_head_html = '<thead><tr>' + table_head_html + '</tr></thead>'
+    table_body_html = '<tbody><tr>' + table_body_html + '</tr></tbody>'
+
+    table_html = '<table class="table table-bordered table-hover" style="font-size: 12px">' + table_head_html + table_body_html + '</table>'
+    
+    return table_html
--- ui/views.py
+++ ui/views.py
@@ -5,6 +5,7 @@ from django.urls import resolve
 from django.contrib.auth.decorators import login_required
 
 from rest_api import models
+from . import utils
 
 # Getting the list of model names in a given app
 _models = apps.get_app_config('rest_api').get_models()
@@ -12,50 +13,6 @@ model_names = list(map(lambda x: x._meta.db_table, _models))
 
 # Getting the list of channels
 channels = models.ChannelInfo.objects.values('channel_name', 'channel_value')
-
-# helper methods
-def make_table_html(myDict):
-    
-    if not myDict:
-        return ''
-    
-    tableHeadHTML = ''
-    tableBodyHTML = ''
-
-    if myDict.get('table'):
-        tableHeadHTML += '<th>Table</th>'
-        tableBodyHTML += '<td>' + myDict.get('table') + '</td>'
-
-    if myDict.get('date'):
-        tableHeadHTML += '<th>Date</th>'
-        tableBodyHTML += '<td>' + myDict.get('date') + '</td>'
-    
-    if myDict.get('start_time') and myDict.get('finish_time'):
-        tableHeadHTML += '<th>Time Interval</th>'
-        tableBodyHTML += '<td>' + myDict.get('start_time') + ' - ' + myDict.get('finish_time') + '</td>'
-    
-    if myDict.get('timezone'):
-        tableHeadHTML += '<th>Timezone</th>'
-        tableBodyHTML += '<td>' + myDict.get('timezone') + '</td>'
-    
-    if myDict.get('channel_names'):
-        tableHeadHTML += '<th>Channel Names</th>'
-        tableBodyHTML += '<td>' + str(myDict.get('channel_names')) + '</td>'
-    
-    if myDict.get('device_id'):
-        tableHeadHTML += '<th>Device</th>'
-        tableBodyHTML += '<td>' + myDict.get('device_id') + '</td>'
-    
-    if myDict.get('request_id'):
-        tableHeadHTML += '<th>Request ID</th>'
-        tableBodyHTML += '<td>' + myDict.get('request_id') + '</td>'
-
-    tableHeadHTML = '<thead><tr>' + tableHeadHTML + '</tr></thead>'
-    tableBodyHTML = '<tbody><tr>' + tableBodyHTML + '</tr></tbody>'
-
-    tableHTML = '<table class="table table-bordered table-hover" style="font-size: 12px">' + tableHeadHTML + tableBodyHTML + '</table>'
-    
-    return tableHTML
 
 # Create your views here.
 @login_required
@@ -65,27 +22,8 @@ def home(request):
 @login_required
 def general(request):
 
-    current_url = resolve(request.path_info).url_name
+    table_dict = utils.make_table_dict(request)
+    table_html = utils.make_table_html(table_dict)
 
-    # myDict = dict(request.GET._iterlists())
-    myDict = dict(request.GET)
-    
-    for key, value in myDict.items():
-        if key != 'channel_values':
-            myDict[key] = value[0]
-    
-    # map channel_values to channel_names and remove the channel_values key
-    if myDict.get('channel_values'):
-        channel_values = [int(x) for x in myDict['channel_values']]
-        channel_names = [t['channel_name'] for t in channels.filter(channel_value__in=channel_values)]
-        myDict['channel_names'] = channel_names
-        myDict.pop("channel_values", None)
-    
-    # Adding table information to dictionary
-    if current_url == 'ui-recording':
-        myDict['table'] =  'Recording'
-    elif current_url == 'ui-blank':
-        myDict['table'] =  'Invalid Frame Tracking'
-
-    return render(request, 'ui/datatable.html', {'tableHTML': make_table_html(myDict)})
+    return render(request, 'ui/datatable.html', {'tableHTML': table_html})
     # return JsonResponse(request.GET)
