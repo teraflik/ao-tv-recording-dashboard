3ef8da93895ffaa22f319e3321fbec0fa6f3d26f
Asutosh Sahoo <asutosh.sahoo@quantiphi.com>
2019-04-12 16:42:36 +0530

    Refactored datatable.js handling the Tabular UI.

--- dashboard/ui/static/ui/js/datatable.js
+++ dashboard/ui/static/ui/js/datatable.js
@@ -1,20 +1,8 @@
-
-/**
- * Set the column names according to the attributes in the JSON Response.
- * @param {string} endpoint - The API endpoint that serves the data.
- * @param {string} headerQuerySelector - querySelector of <thead>
- * @param {string} footerQuerySelector - querySelector of <tfoot>
- * @returns {}
- */
-function setColumnsAsPerData(endpoint, headerQuerySelector, footerQuerySelector) {
-    var data = getJSONSynchronous(endpoint);
-    if (data.length > 0) {
-
-    }
-}
+/*
+1. Include ui/utils.js before loading this.
+*/
 
 function setColumns(columnNames) {
-
     rowHtml = '';
 
     for(var i = 0; i < columnNames.length; i++) {
@@ -27,42 +15,31 @@ function setColumns(columnNames) {
     $('tr#header-row').html(rowHtml);
 }
 
-function videoPathToURL(data) {
-    if (data == "NULL") {
-        return data;
-    }
-    else {
-        return '<a href="'+ data.replace("gs://", "https://storage.cloud.google.com/") + '">Stream</a>';
-    }
-}
-
 $(document).ready(function(){
 
     //  1. obtain API endpoint that fetches data.
     var endpoint = document.URL.replace('ui', 'api');
 
-    //  1. set the basic template for dataTable to work upon.
-    var columnNames;
-    $.ajax({
-        type: 'GET',
-        url: endpoint,
-        async: false,
-        dataType: 'json',
-        success: function(data){
-            if (data.length > 0) {
-                columnNames = Object.keys(data[0]);
-                setColumns(columnNames);
-            }
-        }
-    });
+    //  2. get JSON data.
+    var data = getJSONSynchronous(endpoint);
 
-    if (columnNames) {
+    //  3. if no data received, return.
+    if (data.length == 0) {
+        $('#example').html("<center><h4> No entries for the given set of filters </h4><center>");
+    }
+    else {
+
+        //  1. set column names according to attributes in JSON data.
+        var columnNames = Object.keys(data[0]);
+        setColumns(columnNames);
+    
         //  2. get column names in the dataTable required format
         var columns = [];
         for (i = 0; i < columnNames.length; i++) {
             columns.push({"data": columnNames[i]});
         }
 
+        //  3. create the dataTable object with required configurations
         var table =  $('#example').DataTable({
             "columns": columns,
             "columnDefs": [
@@ -73,23 +50,16 @@ $(document).ready(function(){
                 {
                     "targets": "video_path",
                     "render": function ( data, type, row, meta ) {
-                                return videoPathToURL(data);
+                                if (data == "NULL") {
+                                    return data;
+                                }
+                                else {
+                                    return '<a href="'+ data.replace("gs://", "https://storage.cloud.google.com/") + '">Stream</a>';
+                                }
                     }
                 }
             ],
-            "ajax": {
-                url: endpoint,
-                dataSrc: ''
-            },
+            "data": data
         });
-
-        //  6. refreshing regularly
-        setInterval( function () {
-            console.log("refreshing table");
-            table.ajax.reload(null, false);
-        }, 300000 );
-    }
-    else {
-        $('#example').html("<center><h4> No entries for the given set of filters </h4><center>");
     }
 });
