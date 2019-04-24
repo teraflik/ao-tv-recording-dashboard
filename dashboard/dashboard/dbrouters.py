5aca3258ad397ad253d40f0f410a5cd9a6f725f3
Raghav Khandelwal <raghav.khandelwal@quantiphi.com>
2019-04-24 18:09:21 +0530

    Fix DB Router

--- dashboard/dashboard/dbrouters.py
+++ dashboard/dashboard/dbrouters.py
@@ -21,38 +21,28 @@ class MonitoringDBRouter(object):
     """
     def db_for_read(self, model, **hints):
         "Point all operations on monitoring models to 'db_monitoring'"
-        from django.conf import settings
-        if 'db_monitoring' not in settings.DATABASES:
-            return None
+
         if model._meta.app_label == 'monitoring':
             return 'db_monitoring'
         return None
 
     def db_for_write(self, model, **hints):
         "Point all operations on monitoring models to 'db_monitoring'"
-        from django.conf import settings
-        if 'db_monitoring' not in settings.DATABASES:
-            return None
+
         if model._meta.app_label == 'monitoring':
             return 'db_monitoring'
         return None
 
     def allow_relation(self, obj1, obj2, **hints):
         "Allow any relation if a model in monitoring is involved"
-        from django.conf import settings
-        if 'db_monitoring' not in settings.DATABASES:
-            return None
+
         if obj1._meta.app_label == 'monitoring' or obj2._meta.app_label == 'monitoring':
             return True
         return None
 
-    def allow_syncdb(self, db, model):
+    def allow_migrate(self, db, app_label, model_name=None, **hints):
         "Make sure the monitoring app only appears on the 'monitoring' db"
-        from django.conf import settings
-        if 'db_monitoring' not in settings.DATABASES:
-            return None
-        if db == 'db_monitoring':
-            return model._meta.app_label == 'monitoring'
-        elif model._meta.app_label == 'monitoring':
-            return False
+
+        if app_label == 'monitoring':
+            return db == 'db_monitoring'
         return None
