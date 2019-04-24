91367297ce3db67761c9176d4f68bab2a6b0edab
Raghav Khandelwal <raghav.khandelwal@quantiphi.com>
2019-04-24 15:51:24 +0530

    Fix filter recording db name

--- dashboard/dashboard/dbrouters.py
+++ dashboard/dashboard/dbrouters.py
@@ -6,13 +6,13 @@ class CloudDBRouter(object):
     def db_for_read(self, model, **hints):
         """ reading FilterRecordingTracking from barc-prod """
         if model == FilterRecordingTracking:
-            return 'rest_api'
+            return 'db_rest_api'
         return None
 
     def db_for_write(self, model, **hints):
         """ writing FilterRecordingTracking to barc-prod """
         if model == FilterRecordingTracking:
-            return 'rest_api'
+            return 'db_rest_api'
         return None
 
 class MonitoringDBRouter(object):
